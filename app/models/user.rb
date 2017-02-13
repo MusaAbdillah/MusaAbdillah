afce2e8700ac4351cf84b77dc201e734074236a8
MusaAbdillah <musaabdillahm9@gmail.com>
2017-02-13 08:12:10 +0700

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   belongs_to :role
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  validate :password_complexity
+  before_create :set_default_role
 
-         before_create :set_default_role
+    def password_complexity
+      if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d). /)
+        errors.add :password, "harus mengandung huruf besar, huruf kecil dan angka"
+      end
+    end
 
-         def set_default_role 
-         	self.role ||= Role.find_by_nama('user')
-         end 
+    def set_default_role
+      self.role ||= Role.find_by_nama('user')
+    end
 end
