76a29b2c744c31afe18505c6455330af713ba7da
MusaAbdillah <musaabdillahm9@gmail.com>
2017-03-13 09:29:28 +0700

    change front end

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,8 @@
 class Service < ActiveRecord::Base
    belongs_to :client
    belongs_to :troubel
+   validates :code, :client_id, :troubel_id, presence: true
+   before_save :generate_code
 
    #method untuk menghitung total biaya servis
    def service_price
@@ -13,5 +15,9 @@ class Service < ActiveRecord::Base
    #method untuk print line
    def line
       return "=" * 121
+   end
+
+   def generate_code
+      self[:code] = "#{id}#{SecureRandom.urlsafe_base64(3)}"
    end
 end
--- app/models/troubel.rb
+++ app/models/troubel.rb
@@ -1,4 +1,5 @@
 class Troubel < ActiveRecord::Base
    has_many :services
    has_many :clients, through: :services
+   validates :name, :grade, :price, presence: true
 end
